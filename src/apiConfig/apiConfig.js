264719cdb077c42d519df0f903e6c03df7089d14
priyam1608 <sharmapriyam1608@gmail.com>
2024-09-01 16:42:46 +0530

    login api changed

--- src/apiConfig/apiConfig.js
+++ src/apiConfig/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL = 'http://localhost:8081';
-export const API_RAILWAY_URL= 'https://ecommerce-backend-production-3930.up.railway.app/'
+export const API_RAILWAY_URL= 'https://ecommerce-backend-production-3930.up.railway.app'
 const jwt = localStorage.getItem('jwt');
 
 export const api = axios.create({
