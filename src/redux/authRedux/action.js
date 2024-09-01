da37e995a369f833b3f106d94c279a7bfc50a809
priyam1608 <sharmapriyam1608@gmail.com>
2024-09-01 16:39:18 +0530

    login api changed

--- src/redux/authRedux/action.js
+++ src/redux/authRedux/action.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { api, API_BASE_URL } from "../../apiConfig/apiConfig";
+import { API_BASE_URL, API_RAILWAY_URL } from "../../apiConfig/apiConfig";
 import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./constant";
 
 const registerRequest = () => ({ type: REGISTER_REQUEST });
@@ -10,7 +10,7 @@ const registerFailure = (error) => ({ type: REGISTER_FAILURE, payload: error });
 export const register = (userData) => async (dispatch) => {
   dispatch(registerRequest());
   try {
-    const response = await api.post(`/auth/signup`, userData);
+    const response = await axios.post(`${API_RAILWAY_URL}/auth/signup`, userData);
     const data = response.data;
     console.log(data);
     
@@ -31,7 +31,7 @@ const loginFailure = (error) => ({ type: LOGIN_FAILURE, payload: error });
 export const login = (userData) => async (dispatch) => {
   dispatch(loginRequest());
   try {
-    const response = await api.post(`/auth/signin`, userData);
+    const response = await axios.post(`${API_RAILWAY_URL}/auth/signin`, userData);
     const data = response.data;
     console.log(data);
     
@@ -54,7 +54,11 @@ export const getUserProfile = () => async (dispatch) => {
   const token = localStorage.getItem('jwt');
   dispatch(getUserRequest());
   try {
-    const response = await api.get(`/api/users/profile`);
+    const response = await axios.get(`${API_RAILWAY_URL}/api/users/profile`,{
+        headers:{
+            "Authorization": `Bearer ${token}`
+        }
+    });
     const user = response.data;
     console.log(user);
     
