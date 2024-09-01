3ca1d9fa865cb53227651f0d8381faff42e6ebb4
priyam1608 <sharmapriyam1608@gmail.com>
2024-09-01 15:13:48 +0530

    api changed

--- src/apiConfig/apiConfig.js
+++ src/apiConfig/apiConfig.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export const API_BASE_URL = 'http://localhost:8081';
+export const API_RAILWAY_URL= 'https://ecommerce-backend-production-3930.up.railway.app'
 
 const jwt = localStorage.getItem('jwt');
 
 export const api = axios.create({
-    baseURL:API_BASE_URL,
+    baseURL:API_RAILWAY_URL,
     headers:{
         "Authorization": `Bearer ${jwt}`,
         "Content-Type":"application/json"
